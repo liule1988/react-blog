528f5d7146c3ad1ef2bd09458fdece6333b8fe6e
liule <778252899@qq.com>
2016-12-08 19:06:08 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	diff.xml

--- app/component/artList.js
+++ app/component/artList.js
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import data from '../data/text.json'
+import imgSrc from '../images/pic1.png'
+import '../styles/articlelist.css'
 export default class ArticleList extends React.Component{
 
 
@@ -10,7 +12,14 @@ render(){
     var items = data;
     var renderData = items.map(function (item,index) {
         return(
-            <div key={index}>{item.title}<image resource={require('../images/pic1.png')} style={{width:100,height:100,}}></image></div>
+            <div key={index} className="listWraper">
+                <div className="art_title">
+                    <a href="#"><h2 className="art_title">{item.title}</h2></a>
+                </div>
+                <div className="art_img">
+                    <img src={item.imgUrl} style={{width:50,height:50}}/>
+                </div>
+            </div>
         )
     });
     return(
---
+++ app/pages/welcome.js
@@ -0,0 +1,7 @@
+/**
+ * Created by leliu5 on 2016/12/8.
+ */
+import React from 'react';
+export default class Welcome extends React.Component{
+
+}
