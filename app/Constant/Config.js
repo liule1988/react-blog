2d01b8fbde42bbf49cfd84354366837c3abcf66e
liule <778252899@qq.com>
2016-12-20 01:07:46 +0800

    压缩

--- app/Constant/Config.js
+++ app/Constant/Config.js
@@ -5,6 +5,6 @@ export const CONFIG = {
     blog_name : 'KeepFun',  // 博客名称
     owner: 'liule1988',          // github 用户名
     repo: 'liule.github.io',// 用于存放 blog（issues）的项目名
-    access_token: 'b7b118f848640c3eb5cc8e283d15520d066776cd',          // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    access_token: '2e08bfedac99fc91c'+'2834111bc0f84e63c160c2e',          // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page: '15'                // 默认一页显示几篇文章
 };
