09374e98387cbfbd0fa85168c0c73ad6268d9376
liule1988 <778252899@qq.com>
2017-11-03 00:39:14 +0800

    首页基本架构完成✅

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,17 +3,36 @@
  */
 import React, {Component} from 'react';
 import './index.scss'
+import {Link} from 'react-router-dom'
 class IndexPage extends Component{
     constructor(){
         super()
     }
     render(){
         return(
-            <div className="container">
                 <div className="page page-index">
-                    首页
+                    <div className="index-banner background-fixed">
+                        <div className="le-container">
+                            <h1 className="app-name">KeepFun</h1>
+                            <p className="app-slogan">有些故事还没讲完，那就算了吧</p>
+                            <div className="index-btns">
+                                <Link to="http://www.baidu.com" target="_blank">GitHub</Link>
+                                <Link to="/articles">进入博客</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="index-pages">
+                        <section className="index-section blog-info">
+                            <h2 className="index-pages-title">Hello,React</h2>
+                            <p className="index-pages-slogan">本博客采用 React.js 构建</p>
+                        </section>
+                        <section className="index-section blog-about">
+                            <h2 className="index-pages-title">关于我</h2>
+                            <p className="index-pages-slogan">非著名前端小学僧一枚</p>
+                        </section>
+                    </div>
                 </div>
-            </div>
         )
     }
 }
